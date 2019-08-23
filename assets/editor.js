73eacbd383b4f9373515a5241f582500d5b19e25
FotieMConstant <fotiemoghommahieconstant@gmail.com>
2019-08-23 14:04:41 +0100

    First commit

---
+++ assets/editor.js
@@ -0,0 +1,20 @@
+const first = document.querySelector(".first");
+const iframe = document.querySelector("iframe");
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", () => {
+  var html = first.textContent;
+  iframe.src = "data:text/html;charset=utf-8," + encodeURI(html);
+});
+
+
+first.addEventListener('keyup',()=>{
+  var html = first.textContent;
+  iframe.src = "data:text/html;charset=utf-8," + encodeURI(html);
+})
+
+first.addEventListener("paste", function(e) {
+        e.preventDefault();
+        var text = e.clipboardData.getData("text/plain");
+        document.execCommand("insertText", false, text);
+    });
